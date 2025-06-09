4e5776dfe5bd37a2651e78e88d3d5de7814004f6
Giovani-Miranda <giovanimiranda01@gmail.com>
2025-06-09 16:16:57 -0300

    Class Produto adicionado usando encapsulamento e SRP

--- Prova do gui/Program.cs
+++ Prova do gui/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Produto
+{
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public decimal Preco { get; private set; }
+    public string Categoria { get; private set; }
+
+    public Produto(int id, string nome, decimal preco, string categoria)
+    {
+        if (string.IsNullOrWhiteSpace(nome)) throw new ArgumentException("Nome inválido");
+        if (preco <= 0) throw new ArgumentException("Preço deve ser positivo");
+
+        Id = id;
+        Nome = nome;
+        Preco = preco;
+        Categoria = categoria;
+    }
+}
